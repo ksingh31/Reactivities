f8a6819a0000dda4c5027a4820d42e06cf4eb8eb
ksingh31 <karanbirsingh667@gmail.com>
2021-02-08 03:22:09 -0700

    Display activities and styling

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Button, Icon, Image } from 'semantic-ui-react'
+import Card from 'semantic-ui-react/dist/commonjs/views/Card'
+import { IActivity } from '../../../app/models/activity'
+
+interface IProps {
+    activity: IActivity;
+}
+
+const ActivityDetails: React.FC<IProps> = ({activity}) => {
+    return (
+        <Card fluid>
+            <Image src={`/assets/categoryImages/${activity.category}.jpg`} wrapped ui={false} />
+            <Card.Content>
+            <Card.Header>{activity.title}</Card.Header>
+            <Card.Meta>
+                <span>{activity.date}</span>
+            </Card.Meta>
+            <Card.Description>
+                {activity.description}
+            </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button.Group width={2}>
+                    <Button basic color='blue' content='Edit' />
+                    <Button basic color='grey' content='Cancel' />
+                </Button.Group>
+            </Card.Content>
+        </Card>
+    )
+}
+
+export default ActivityDetails
